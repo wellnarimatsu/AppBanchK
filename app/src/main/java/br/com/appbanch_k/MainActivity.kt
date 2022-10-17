bcc20edb777eca5d6c766595049b22288a9dad27
wellison <wellison.narimatsu@outlook.com>
2022-10-17 12:21:54 -0300

    Inserindo SplashScreen + Icone

--- app/src/main/java/br/com/appbanch_k/MainActivity.kt
+++ app/src/main/java/br/com/appbanch_k/MainActivity.kt
@@ -27,6 +27,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AppBanch_K)
+
         setContentView(R.layout.activity_main)
 
         title="BanchK"
